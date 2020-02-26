f972b1dde711ff77c9bba010d9c01f9462ec9d55
neilhendricks <neilhendricks@hotmail.com>
2020-02-27 01:56:06 +0400

    First commit for Neil's Edamam Recipe app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import { REACT_APP_API_ID, REACT_APP_API_KEY} from './config.js';
+import Recipe from "./Recipe"
 import './App.css';
+//require("dotenv").config()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const APP_ID = REACT_APP_API_ID;
+  const APP_KEY = REACT_APP_API_KEY;  
+  
+  const [recipes, setRecipes] = useState([]); //taking arguments from the state, passing down to props
+  const [search, setSearch] = useState("");// updates everytime we type a value into search. We use query below to fix
+  const [query, setQuery] = useState("chicken"); //only updates search after we click submit button
+
+  useEffect(()=>{
+    getRecipes();
+  }, [query]); // useEffect only runs when our query changes
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  // e is an event, update search is a function that changes the state
+  const updateSearch = e=> {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+
+
+  return( //returns jsx
+    <div className = "App">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe =>(
+        <Recipe // props get values passed down from state, the go to Recipe.js
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
